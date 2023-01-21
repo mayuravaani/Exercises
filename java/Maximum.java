b461b0f45d8b40acbda2ab3946976372d5dfcc8c
tharindu021 <tharinduniroshan4@gmail.com>
2023-01-21 11:51:04 +0530

    update Maximum.java file and Maxima.class file

--- java/Maximum.java
+++ java/Maximum.java
@@ -1,5 +1,14 @@
-/*
-Problem:
-You are given N integers length array by the user. Find the maximum number
-
-*/
+class mamima {
+    public static void main(String args[])
+    {
+        int [] array1 = {2,54,65,34,56,54,73,34,2};
+        int max = 0;
+        for(int i = 0;i < array1.length;i++)
+        {
+            if(array1[i] >max){
+                max = array1[i];
+            }
+        }
+        System.out.println("Maximum Number Of Array1:"+max);
+    }
+}

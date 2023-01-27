54c84fc8ad623f9318e0a9a1aef5d3173ed1c940
Mohamed Kamaludeen <106368079+Mohamedkamaludeen@users.noreply.github.com>
2023-01-28 00:28:56 +0530

    Update Circle.java

--- java/Circle.java
+++ java/Circle.java
@@ -3,3 +3,14 @@ Problem:
 Write a Java program to print the area and perimeter of a circle
 
 */
+class numbers{
+    public static void main(String args[])
+    {
+        int radius=10;
+        int area=(22/7)*radius*radius;
+        iint perimeter=2*(22/7)*radius;
+        System.out.println("The x= "+x+"and y sum is= "+sum);
+        System.out.println("The x= "+x+"and y multiply is= "+multiply);
+       //sdds
+    }
+}

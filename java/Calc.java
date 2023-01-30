5356a74206338cb904703c48fb15a9ef243ab2e1
Kushan991029 <kushansrathnayaka99@gmail.com>
2023-01-30 23:59:33 +0530

    2019csc038

--- java/Calc.java
+++ java/Calc.java
@@ -1,7 +1,18 @@
-/*
-Problem:
-Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
-
-*/
+class test
+{
+	public static void main(String args[])
+	{
+		int a = 10;
+		int b = 5;
+		
+		System.out.println("Sum : "+(a+b));
+		
+		System.out.println("Multiply : "+(a*b));
+		
+		System.out.println("Substract : "+(a-b));
+		
+		System.out.println("Divide : "+(a/b));
+	}
+}
 
 

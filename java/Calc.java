8f4f9586cf00109500932adfc9b918a1d32e5c12
Farook Sajith Ahamed <107676277+FarookSajithAhamed@users.noreply.github.com>
2023-01-30 16:37:40 +0530

    2019csc004

--- java/Calc.java
+++ java/Calc.java
@@ -3,7 +3,7 @@ Problem:
 Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
 
 */
-class test
+class test1
 {
   public static void main(String args[])
   {

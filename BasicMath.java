52f8c903b68a35eee5440aaa8b2c37228298a11b
MDSDimuthu <senaldimuthu12345@gmail.com>
2023-01-31 22:38:09 +0530

    Added java file

---
+++ BasicMath.java
@@ -0,0 +1,41 @@
+Class Math
+{
+    public void Addition(int x,int y)
+    {
+        return x+y;
+    }
+
+
+    public void Subtract(int x,int y)
+    {
+        return x-y;
+    }
+
+
+    public void multiply(int x,int y)
+    {
+        return x*y;
+    }
+
+    public void Divide(int x,int y)
+    {
+        return x/y;
+    }
+
+
+    public void Reminder(int x,int y)
+    {
+        return x%y;
+    }
+
+
+    public static void main(String args[])
+    {
+        System.out.println(Addition(5,7));
+        System.out.println(Subtract(5,7));
+        System.out.println(multiply(5,7));
+        System.out.println(Divide(14,7));
+        System.out.println(Reminder(10,7));
+
+    }
+}

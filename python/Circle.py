9fcff2965ce1f9362b57e44bbe701678e2c33470
inkblast <ies.imeshnuwanthak@gmail.com>
2023-01-31 22:18:07 +0530

    2019csc018

--- python/Circle.py
+++ python/Circle.py
@@ -3,3 +3,14 @@ Problem:
 Write a python program to print the area and perimeter of a circle
 
 */
+
+
+import math
+
+r = int(input("Enter the radius: "))
+
+area = math.pi * r * r
+perimeter = 2 * math.pi * r
+
+
+print("area: ",area,"\nperimeter: ",perimeter)

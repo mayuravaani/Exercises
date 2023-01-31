813fef99bec2f5e238b69936c53a627a4c0b419f
Pasindu1998 <pasindu.ugc@gmail.com>
2023-01-31 22:26:09 +0530

    2019csc042

--- java/Calc.java
+++ java/Calc.java
@@ -4,4 +4,27 @@ Write a Java program to print the sum (addition), multiply, subtract, divide and
 
 */
 
+class exercises
+{
+ public static void main(String args[])
+ {
+    int a=2;
+   int b=3;
+   int c=a+b;
+   
+   System.out.println("addition is:"+c);
+   
+   int d=a*b;
+   System.out.println("multiplication is:"+d);
+   
+   int e=a%b;
+   System.out.println("reminder is:"+e);
+   
+   int f=a-b;
+   System.out.println("subtract is:"+f);
+   
+   int g=a/b;
+   System.out.println("divition is:"+g);
+ }
+}
 

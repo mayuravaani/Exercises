0287f679a1cf99d7151d84a199a4f89168fe9eb4
98Chanaka <chanud1998@gmail.com>
2023-01-31 21:49:47 +0530

    2019CSC037

--- java/Calc.java
+++ java/Calc.java
@@ -5,3 +5,17 @@ Write a Java program to print the sum (addition), multiply, subtract, divide and
 */
 
 
+class Calc{
+	public static void main(String args[])
+	{
+		int x = 5;
+		int y = 4;
+		
+		System.out.println("Sum : " + (x+y) );
+		System.out.println("Multiply : " + (x*y) );
+		System.out.println("Substract : " + (x-y) );
+		System.out.println("Divide  : " + (x/y) );
+	}
+}
+
+
